252d47df946e34f2e15b8bbf8220e8649c94144d
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-04-21 19:25:52 +0200

    Add logic to input field and submit button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    const target = e.target;
+
+    setEnteredPlayerName(target.value)
+  }
+
+  const handleClick = (() => {
+    setSubmitted(true)
+  })
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { submitted && enteredPlayerName ? enteredPlayerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
