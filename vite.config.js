315ba0114178e66ec630f0f7fe2ed294c1b330f6
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-04-21 18:24:25 +0200

    Update vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  let base = '/'
+
+  if (mode === 'test') {
+    base = '/the-almost-final-countdown/'
+  }
+
+  return {
+    plugins: [react()],
+    base,
+  }
 })
